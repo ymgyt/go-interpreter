3136cd0f67c62c28a8ac4b81e617ba939228dc4a
ymgyt <yamaguchi7073xtt@gmail.com>
2018-08-29 17:07:02 +0900

    chapter 2.4

---
+++ parser/parser_test.go
@@ -0,0 +1,79 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/ymgyt/go-interpreter/ast"
+	"github.com/ymgyt/go-interpreter/lexer"
+	"github.com/ymgyt/go-interpreter/parser"
+)
+
+func TestLetStatement(t *testing.T) {
+	input := `
+let x  5;
+let y = 10;
+let foobar = 65536;
+`
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got =%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		want string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.want) {
+			return
+		}
+	}
+}
+
+func checkParseErrors(t *testing.T, p *parser.Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, e := range errors {
+		t.Errorf("parser error: %s", e)
+	}
+	t.FailNow()
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+	}
+
+	return true
+}
